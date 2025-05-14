463a32e97c9731060bbc3557a371674314623ad4
annak <annak@10.16.16.106>
2025-05-14 11:42:03 -0400

    second commit

---
+++ src/projectmanagementformative/Notes.java
@@ -0,0 +1,35 @@
+/*
+ Notes class that has a title of note and some content
+ */
+package projectmanagementformative;
+
+/**
+ *
+ * @author annak
+ */
+public class Notes {
+    private String topic;
+    private String content;
+    public Notes(){
+        topic="";
+        content="";
+    }
+    public Notes(String topic, String content){
+        this();
+        this.topic=topic;
+        this.content=content;
+    }
+    
+    public void setTopic(String topic){
+        this.topic=topic;
+    }
+    public String getTopic(){
+        return topic;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+    public String getContent(){
+        return content;
+    }
+}
